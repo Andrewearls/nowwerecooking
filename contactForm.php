79f95c90964913f0d21b91b40d51f7e551513d06
unknown <andrewearls@rocketmail.com>
2018-05-21 16:50:24 -0700

    mail hopefully works

--- contactForm.php
+++ contactForm.php
@@ -1,11 +1,17 @@
 <?php
+
    // the message
 	$msg = "First line of text\nSecond line of text";
 
 	// use wordwrap() if lines are longer than 70 characters
 	$msg = wordwrap($msg,70);
 
+	$headers = 'MIME-Version: 1.0' . "\r\n";
+	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+	$headers .= 'From: andrew@nowwerecooking.io' . "\r\n";
+	$headers .= 'To: andrew@nowwerecooking.io'."\r\n";
+
 	// send email
-	echo mail("andrew@nowwerecooking.io","My subject",$msg);
+	echo mail("andrew@nowwerecooking.io","My subject",$msg, $headers);
    exit("made it here");
 ?>
