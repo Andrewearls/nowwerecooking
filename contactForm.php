57ba94c3825025392cf2d8c592b8dfb92e63b01c
unknown <andrewearls@rocketmail.com>
2018-05-21 16:57:14 -0700

    added donotreply email

--- contactForm.php
+++ contactForm.php
@@ -8,7 +8,7 @@
 
 	$headers = 'MIME-Version: 1.0' . "\r\n";
 	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-	$headers .= 'From: andrew@nowwerecooking.io' . "\r\n";
+	$headers .= 'From: donotreply@nowwerecooking.io' . "\r\n";
 	$headers .= 'To: andrew@nowwerecooking.io'."\r\n";
 
 	// send email
